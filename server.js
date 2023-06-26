e8cdd45f616c6cdfbfad5ac10039747f81b9d736
Jiasisi <lijiasi1202@gmail.com>
2023-06-26 18:44:00 -0300

    change a server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const apiRouter = require('./Develop/routers/apiRouter');
 const homeRouter = require('./Develop/routers/homeRouter')
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
